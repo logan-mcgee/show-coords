ce7b277535155469c05ddaa939dd25ec31f49e2b
Darrian <darriangaylor@icloud.com>
2021-03-18 18:40:26 -0400

    Just a command to toggle coords displaying.

--- coords.lua
+++ coords.lua
@@ -1,3 +1,13 @@
+local showCoords = false
+
+RegisterCommand('showCoords', function()
+	if not showCoords then
+		showCoords = true
+	else
+		showCoords = false
+	end
+end, false)
+
 function DrawTxt(text, x, y)
 	SetTextFont(0)
 	SetTextProportional(1)
@@ -13,54 +23,58 @@ end
 
 Citizen.CreateThread(function()
     while true do
-		Citizen.Wait(0)
-		local entity = IsPedInAnyVehicle(PlayerPedId()) and GetVehiclePedIsIn(PlayerPedId(), false) or PlayerPedId()
-		x, y, z = table.unpack(GetEntityCoords(entity, true))
+		if showCoords then
+			Citizen.Wait(0)
+			local entity = IsPedInAnyVehicle(PlayerPedId()) and GetVehiclePedIsIn(PlayerPedId(), false) or PlayerPedId()
+			x, y, z = table.unpack(GetEntityCoords(entity, true))
+			
+			roundx = tonumber(string.format("%.2f", x))
+			roundy = tonumber(string.format("%.2f", y))
+			roundz = tonumber(string.format("%.2f", z))
+			
+			DrawTxt("~r~X:~s~ "..roundx, 0.32, 0.00)
+			DrawTxt("~r~Y:~s~ "..roundy, 0.38, 0.00)
+			DrawTxt("~r~Z:~s~ "..roundz, 0.445, 0.00)
+
+			heading = GetEntityHeading(entity)
+			roundh = tonumber(string.format("%.2f", heading))
+			DrawTxt("~r~H:~s~ "..roundh, 0.50, 0.00)
+
+			local rx,ry,rz = table.unpack(GetEntityRotation(PlayerPedId(), 1))
+			DrawTxt("~r~RX:~s~ "..tonumber(string.format("%.2f", rx)), 0.38, 0.03)
+			DrawTxt("~r~RY:~s~ "..tonumber(string.format("%.2f", ry)), 0.44, 0.03)
+			DrawTxt("~r~RZ:~s~ "..tonumber(string.format("%.2f", rz)), 0.495, 0.03)
 		
-		roundx = tonumber(string.format("%.2f", x))
-		roundy = tonumber(string.format("%.2f", y))
-		roundz = tonumber(string.format("%.2f", z))
-		
-		DrawTxt("~r~X:~s~ "..roundx, 0.32, 0.00)
-		DrawTxt("~r~Y:~s~ "..roundy, 0.38, 0.00)
-		DrawTxt("~r~Z:~s~ "..roundz, 0.445, 0.00)
+			speed = GetEntitySpeed(PlayerPedId())
+			rounds = tonumber(string.format("%.2f", speed))
+			DrawTxt("~r~Player Speed: ~s~"..rounds, 0.40, 0.92)
 
-		heading = GetEntityHeading(entity)
-		roundh = tonumber(string.format("%.2f", heading))
-		DrawTxt("~r~H:~s~ "..roundh, 0.50, 0.00)
+			health = GetEntityHealth(PlayerPedId())
+			DrawTxt("~r~Player Health: ~s~"..health, 0.40, 0.95)
 
-        local rx,ry,rz = table.unpack(GetEntityRotation(PlayerPedId(), 1))
-		DrawTxt("~r~RX:~s~ "..tonumber(string.format("%.2f", rx)), 0.38, 0.03)
-		DrawTxt("~r~RY:~s~ "..tonumber(string.format("%.2f", ry)), 0.44, 0.03)
-		DrawTxt("~r~RZ:~s~ "..tonumber(string.format("%.2f", rz)), 0.495, 0.03)
-	
-		speed = GetEntitySpeed(PlayerPedId())
-		rounds = tonumber(string.format("%.2f", speed))
-		DrawTxt("~r~Player Speed: ~s~"..rounds, 0.40, 0.92)
+			camRotX = GetGameplayCamRot().x
+			DrawTxt("~r~CR X: ~s~"..tonumber(string.format("%.2f", camRotX)), 0.36, 0.88)
 
-		health = GetEntityHealth(PlayerPedId())
-		DrawTxt("~r~Player Health: ~s~"..health, 0.40, 0.95)
+			camRotY = GetGameplayCamRot().y
+			DrawTxt("~r~CR Y: ~s~"..tonumber(string.format("%.2f", camRotY)), 0.44, 0.88)
 
-		camRotX = GetGameplayCamRot().x
-		DrawTxt("~r~CR X: ~s~"..tonumber(string.format("%.2f", camRotX)), 0.36, 0.88)
+			camRotZ = GetGameplayCamRot().z
+			DrawTxt("~r~CR Z: ~s~"..tonumber(string.format("%.2f", camRotZ)), 0.51, 0.88)
 
-		camRotY = GetGameplayCamRot().y
-		DrawTxt("~r~CR Y: ~s~"..tonumber(string.format("%.2f", camRotY)), 0.44, 0.88)
+			veheng = GetVehicleEngineHealth(GetVehiclePedIsUsing(PlayerPedId()))
+			vehbody = GetVehicleBodyHealth(GetVehiclePedIsUsing(PlayerPedId()))
+			if IsPedInAnyVehicle(PlayerPedId(), 1) then
+				vehenground = tonumber(string.format("%.2f", veheng))
+				vehbodround = tonumber(string.format("%.2f", vehbody))
 
-		camRotZ = GetGameplayCamRot().z
-		DrawTxt("~r~CR Z: ~s~"..tonumber(string.format("%.2f", camRotZ)), 0.51, 0.88)
+				DrawTxt("~r~Engine Health: ~s~"..vehenground, 0.015, 0.76)
 
-		veheng = GetVehicleEngineHealth(GetVehiclePedIsUsing(PlayerPedId()))
-		vehbody = GetVehicleBodyHealth(GetVehiclePedIsUsing(PlayerPedId()))
-		if IsPedInAnyVehicle(PlayerPedId(), 1) then
-			vehenground = tonumber(string.format("%.2f", veheng))
-			vehbodround = tonumber(string.format("%.2f", vehbody))
+				DrawTxt("~r~Body Health: ~s~"..vehbodround, 0.015, 0.73)
 
-			DrawTxt("~r~Engine Health: ~s~"..vehenground, 0.015, 0.76)
-
-			DrawTxt("~r~Body Health: ~s~"..vehbodround, 0.015, 0.73)
-
-			DrawTxt("~r~Vehicle Fuel: ~s~"..tonumber(string.format("%.2f", GetVehicleFuelLevel(GetVehiclePedIsUsing(PlayerPedId())))), 0.015, 0.70)
+				DrawTxt("~r~Vehicle Fuel: ~s~"..tonumber(string.format("%.2f", GetVehicleFuelLevel(GetVehiclePedIsUsing(PlayerPedId())))), 0.015, 0.70)
+			end
+		else
+			Citizen.Wait(1000)
 		end
     end
 end)
